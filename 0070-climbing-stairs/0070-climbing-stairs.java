b5b1b01046a24a955106805e04ca87142fceea75
SamArlinJeffry <samarlinjeffry726@gmail.com>
2025-04-06 11:34:27 +0530

    Time: 0 ms (100%), Space: 40.9 MB (10.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1||n==0)return 1;
+        if(n==1)return 1;
         int arr[]=new int[n+1];
         arr[0]=0;
         arr[1]=1;
