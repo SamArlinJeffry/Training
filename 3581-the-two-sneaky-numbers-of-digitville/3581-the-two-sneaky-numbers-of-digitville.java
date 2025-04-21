c3b82e6ed32dcc2147bababd32afc7068c94d004
SamArlinJeffry <samarlinjeffry726@gmail.com>
2025-04-21 10:28:11 +0530

    Time: 2 ms (63.35%), Space: 45.1 MB (16.61%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Arrays.sort(nums);
+        int[]arr=new int[2];
+        int j=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                arr[j++]=nums[i];
+                i++;
+                if(j>1)break;
+            }
+        }
+        return arr;
+    }
+}
