7c799ad12c8826b83bef968fc8d28f2d8d27840e
SamArlinJeffry <samarlinjeffry726@gmail.com>
2025-07-23 16:03:41 +0530

    Time: 2 ms (34.94%), Space: 44.4 MB (80.42%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
@@ -3,23 +3,13 @@ class Solution {
         int sum=0;
         for(int i=0;i<nums.size();i++){
             String s=Integer.toBinaryString(i);
-            // System.out.println(i+"  " + s);
-            if(iscorr(s,k)==true){
+            int x=Integer.bitCount(i);
+            if(x==k){
                 sum+=nums.get(i);
             }
             
         }
         return sum;
     }
-    public Boolean iscorr(String s,int k){
-        int count=0,j=0;
-            while(j<s.length()){
-                if(s.charAt(j)=='1'){
-                    count++;
-                }
-                j++;
-            }
-            if(count==k)return true;
-            return false;
-    }
+    
 }
